36b93cc36a47fe88098133f3de33c8cab6f2ec54
Noa Verner <noa.verner@post.runi.ac.il>
2025-11-05 09:56:34 +0200

    Solved all tasks

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,14 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
-	}
+    public static void main(String[] args){
+        int currentValue = Integer.parseInt(args[0]);
+        double ratePercent = Double.parseDouble(args[1]);
+        int years = Integer.parseInt(args[2]);
+
+        double rate = ratePercent / 100.0;
+        double futureValue = currentValue * Math.pow(1 + rate, years);
+
+        System.out.println("After " + years + " years, $" + currentValue +
+                           " saved at " + ratePercent + "% will yield $" + (int) futureValue);
+    }
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -1,17 +1,29 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
+	private static final int HOURS_AT_A_DAY = 24;
+	private static final int HOURS_AT_HALF_A_DAY = HOURS_AT_A_DAY/2;
+	private static final String AM = "AM";
+	private static final String PM = "PM";
+
 	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
 		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
+		// It concatenates the empty string "" with the leftmost hour-digit.
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		
+		// Swith to 12 hour format
+		String timePeriodSuffix = AM;
+		if (hours >= HOURS_AT_HALF_A_DAY) {
+			timePeriodSuffix = PM;
+			if (hours > HOURS_AT_HALF_A_DAY) {
+				hours = hours - HOURS_AT_HALF_A_DAY;
+			}
+		}
+
+		System.out.printf("%d:%02d %s%n", hours, minutes, timePeriodSuffix);
 	}
+
 }
